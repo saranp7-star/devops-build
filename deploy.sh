f2f3f8f5b1f7dd2b90fa843bd04d7477b5f5dd7b
root <root@ip-172-31-90-212.ec2.internal>
2024-12-10 11:06:53 +0000

    upadtedockercred

--- deploy.sh
+++ deploy.sh
@@ -3,14 +3,15 @@ if [[ $GIT_BRANCH == "origin/dev" ]]; then
     # Build your project
     sh 'chmod +x build.sh'
     sh './build.sh'
-    docker login -u saranp7 -p dckr_pat_5WoqzHtwpiu3pkwVApO7nN5_qoM
+    docker login -u saranp7 -p "$DOCKER_PAT"
     docker tag test saranp7/dev
     docker push saranp7/dev
 
 elif [[ $GIT_BRANCH == "origin/main" ]]; then
     sh 'chmod +x build.sh'
     sh './build.sh'
-    docker login -u saranp7 -p dckr_pat_5WoqzHtwpiu3pkwVApO7nN5_qoM
-    docker tag test saranp7/prod 
+    docker login -u saranp7 -p "$DOCKER_PAT"
+    docker tag test saranp7/prod
     docker push saranp7/prod
 fi
+
