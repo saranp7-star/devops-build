7d93ac7035d230951d787cae67dd3f17c0fa3c68
root <root@ip-172-31-90-212.ec2.internal>
2024-12-10 10:30:37 +0000

    pushin scripts

--- deploy.sh
+++ deploy.sh
@@ -3,14 +3,14 @@ if [[ $GIT_BRANCH == "origin/dev" ]]; then
     # Build your project
     sh 'chmod +x build.sh'
     sh './build.sh'
-    docker login -u ar8888 -p dckr_pat_EADWHoJ3ZHIAazRONGu5WkNt0EY
-    docker tag test ar8888/dev
-    docker push ar8888/dev
+    docker login -u saranp7 -p dckr_pat_5WoqzHtwpiu3pkwVApO7nN5_qoM
+    docker tag test saranp7/dev
+    docker push saranp7/dev
 
 elif [[ $GIT_BRANCH == "origin/main" ]]; then
     sh 'chmod +x build.sh'
     sh './build.sh'
-    docker login -u ar8888 -p dckr_pat_EADWHoJ3ZHIAazRONGu5WkNt0EY
-    docker tag test ar8888/prod 
-    docker push ar8888/prod
+    docker login -u saranp7 -p dckr_pat_5WoqzHtwpiu3pkwVApO7nN5_qoM
+    docker tag test saranp7/prod 
+    docker push saranp7/prod
 fi
